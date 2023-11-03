91e2b96b4ef1e5a35b109c704c8de736aed1e04c
nuwansamaware <nuwansam@awarelabs.io>
2023-11-03 14:35:59 +1100

    Add tracked tests support

--- src/frontend/cypress/support/e2e.js
+++ src/frontend/cypress/support/e2e.js
@@ -15,25 +15,5 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-//const enableTracking = require('tracked-tests-cypress')
-const { v4: uuidv4 } = require('uuid');
-
-    beforeEach(() => {
-        const uniqueUUID = uuidv4();
-        const titleParts = Cypress.currentTest.titlePath;
-        cy.intercept(
-          { url: '*', middleware: true },
-          (req) => {
-            if (titleParts && titleParts.length >= 2 && titleParts[1]!='"after all" hook') {
-              req.headers['trackedtest.suite'] = titleParts[0];
-              req.headers['trackedtest.name'] = titleParts[1];
-              req.headers['trackedtest.invocation_id'] = uniqueUUID;
-              req.headers['trackedtest.type'] = 'cypress';
-//              if(!disableLogging){
-                console.log("Tracked Test metadata attached" + " suite: " + req.headers['trackedtest.suite'] + " name: " + req.headers['trackedtest.name'] + " invocation_id: " + uniqueUUID);
-//              }
-            }
-          }
-        ).as("modifiedRequest");
-
-    });
+const enableTracking = require('tracked-tests-cypress')
+enableTracking();
